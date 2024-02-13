f70afe3aceb741a4e4ffba86fd5d0bc7f14b207b
Oliver Fields <oliver@phnd.net>
2024-02-13 11:05:02 +0100

    Deploy

--- shortcodes.py
+++ shortcodes.py
@@ -6,8 +6,6 @@ def benk_image_path(site, page, image_class):
 
 	img_cache_name = 'benk-' + img_name + '_' + image_class + '.jpg'
 
-	print(img_cache_name)
-
 	# Return if we have this cached already
 	try:
 		return page.cache[img_cache_name]
@@ -24,10 +22,33 @@ def benk_image_path(site, page, image_class):
 	img_path_full = site.content_dir + img_path_relative_url
 
 	# Create image by class
-	print(img_path_full)
 	img_tag = site.shortcodes['image'](site, page, img_path_full, 'Benk', image_class=image_class)
 
 	# Save to cache
 	page.cache[img_cache_name] = img_path_relative_url_class
 
 	return page.cache[img_cache_name]
+
+def opengraph(site, page):
+	img_url = site.base_url + benk_image_path(site, page, 'desktop')
+	desc = str(page.headers['description'])
+
+	domain = site.base_url.replace('https://', '')
+	domain = site.base_url.replace('http://', '')
+
+	# Facebook
+	html = '<meta property="og:url" content="' + page.absolute_url + '" />\n'
+	html += '<meta property="og:title" content="' + page.title + '" />\n'
+	html += '<meta property="og:description" content="' + desc + '" />\n'
+	html += '<meta property="og:image" content="' + img_url + '" />\n'
+
+	# Twitter
+	html += '<meta name="twitter:card" content="summary_large_image" />\n'
+	html += '<meta property="twitter:domain" content="' + domain + '" />\n'
+	html += '<meta property="twitter:url" content="' + page.absolute_url + '" />\n'
+	html += '<meta name="twitter:title" content="' + page.title + '" />\n'
+	html += '<meta name="twitter:description" content="' + desc + '" />\n'
+	html += '<meta name="twitter:image" content="' + img_url + '" />\n'
+
+	return html
+
--- themes/benker/templates/base.mako
+++ themes/benker/templates/base.mako
@@ -5,14 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/assets/theme/favicon.ico" />
+    <link rel="canonical" href="${page.absolute_url}" />
     <title>${page.title}</title>
+    <meta name="description" content="${page.headers['description']}"/>
     <base href="${site.base_url}" />
     <meta name="Generator" content="pagegen.phnd.net" />
     <link rel="stylesheet" href="${site.base_url}/assets/theme/site.css" type="text/css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     % if page.headers['template'] == 'benk.mako':
-    <meta property="fb:admins" content="oliverjfields"/>
+      <meta property="fb:admins" content="oliverjfields"/>
+      <% og = site.shortcodes['opengraph'](site, page) %>
+      ${og}
+    % elif page.headers['template'] == 'homepage.mako':
+      <% og = site.shortcodes['opengraph'](site, page) %>
+      ${og}
     % endif
   </head>
   <body>
