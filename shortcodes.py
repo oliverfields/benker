007f421f4308b0814c5973ba9328fc6aa0060be4
Oliver Fields <oliver@phnd.net>
2024-09-09 19:16:13 +0200

    tweak

--- shortcodes.py
+++ shortcodes.py
@@ -1,7 +1,5 @@
 def benk_image_path(site, page, image_class):
 
-    return ''
-
     # Get page name without path and extension
     img_name = page.source_path.rpartition('/')[2]
     img_name = img_name.rpartition('.')[0]
@@ -23,11 +21,17 @@ def benk_image_path(site, page, image_class):
     img_path_relative_url_class = '/assets/' + img_cache_name
     img_path_full = site.content_dir + img_path_relative_url
 
+
     # Create image by class
-    img_tag = site.shortcodes['image'](site, page, img_path_full, 'Benk', image_class=image_class)
+    try:
+        img_tag = site.shortcodes['image'](site, page, img_path_full, 'Benk', image_class=image_class)
+        print(img_tag)
+    except:
+        img_tag = ''
 
     # Save to cache
     page.cache[img_cache_name] = img_path_relative_url_class
+
 
     return page.cache[img_cache_name]
 
